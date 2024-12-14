4ddae855330079bdf9355e222f8446eb5cd4e4a0
grygoriev <vovagrigoriev@gmail.com>
2024-12-14 11:20:41 +0200

    task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,80 @@
-// Данный импорт - декларативное описание зависимости
-// (указывает, какой ресурс нужен, но не говорит, "как" именно его обрабатывать).
-import ReactLogo from './assets/react.svg?react';
-import { createElement } from 'react';
+import styles from './App.module.css';
+import { useState } from 'react';
 
 export const App = () => {
-	// Начинается декларативный стиль: мы описываем желаемую структуру,
-	// а не говорим пошагово, как её создавать или изменять.
-	return createElement(
-		'div',
-		{ className: 'container' },
-		createElement('h1', null, 'Hello World!'),
-		createElement(ReactLogo), // Декларативно вставляем компонент
-		createElement('hr'),
-		// Здесь вызывается метод new Date().getFullYear().
-		// Сам вызов — императивен, но результат используется в декларативном описании интерфейса.
-		createElement('span', { className: 'span-year' }, new Date().getFullYear()),
+	const [value, setValue] = useState('');
+	const [list, setList] = useState([]);
+	const [error, setError] = useState('');
+
+	const onInputButtonClick = () => {
+		const promptValue = prompt('Введите значение');
+		setValue(promptValue);
+		if (promptValue.length > 2) {
+			setError('');
+		} else {
+			setError('Введенное значение должно содержать минимум 3 символа');
+		}
+	};
+
+	const isValueValid = value.length > 2;
+
+	const formatDate = (date) => date.toLocaleString().replace(',', '');
+
+	const onAddButtonClick = () => {
+		if (isValueValid) {
+			const id = Date.now();
+			// const updatedList = [...list, { id: id, value: value }];
+			// setList(updatedList);
+			setList((list) => [
+				...list,
+				{ id: id, value: value, date: formatDate(new Date()) },
+			]);
+			setError('');
+			setValue('');
+		}
+	};
+
+	const errorBlock = <div className={styles.error}>{error}</div>;
+
+	const listBlock = (
+		<ul className={styles.list}>
+			{list.map(({ id, value, date }) => (
+				<li className={styles.listItem} key={id}>
+					{value} ({date})
+				</li>
+			))}
+		</ul>
 	);
-	// На этом месте декларативный стиль для UI завершается.
+
+	const noElementsMsg = (
+		<p className={styles.noMarginText}>Нет добавленных элементов</p>
+	);
+
+	return (
+		<div className={styles.app}>
+			<h1 className={styles.pageHeading}>Ввод значения</h1>
+			<p className={styles.noMarginText}>
+				Текущее значение <code>value</code>: &#34;
+				<output className={styles.currentValue}>{value}</output>
+				&#34;
+			</p>
+			{error !== '' ? errorBlock : null}
+			<div className={styles.buttonsContainer}>
+				<button className={styles.button} onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
+				<button
+					className={styles.button}
+					disabled={!isValueValid}
+					onClick={onAddButtonClick}
+				>
+					Добавить в список
+				</button>
+			</div>
+			<div className={styles.listContainer}>
+				<h2 className={styles.listHeading}>Список:</h2>
+				{list.length > 0 ? listBlock : noElementsMsg}
+			</div>
+		</div>
+	);
 };
